3d575fd95e425f6555872fe9918951ed48d3b06e
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-15 15:20:50 +0530

    Header Section Completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your Favourite Food Here.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam vero ullam deserunt, odio, corrupti aliquam cum consequatur aspernatur autem voluptate dolores veniam repudiandae aut suscipit tempore quae nemo sequi.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
