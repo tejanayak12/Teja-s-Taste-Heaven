aa72cb240b5800a08cc7a60aa8a7aa8c6e8abaff
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-28 17:01:06 +0530

    Mu Orders Page Completed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -60,6 +60,7 @@ import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
+import parcel from './parcel.png'
 
 export const assets = {
     logo,
@@ -81,7 +82,8 @@ export const assets = {
     profile_icon,
     logout_icon,
     bag_icon,
-    parcel_icon
+    parcel_icon,
+    parcel
 }
 
 export const menu_list = [
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
-const PlaceOrder = () => {
 
+const PlaceOrder = () => {
     const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
 
     const [data, setData] = useState({
@@ -27,30 +28,39 @@ const PlaceOrder = () => {
     const placeOrder = async (event) => {
         event.preventDefault();
         let orderItems = [];
-        food_list.map((item) => {
+        food_list.forEach((item) => {
             if (cartItems[item._id] > 0) {
                 let itemInfo = item;
                 itemInfo["quantity"] = cartItems[item._id];
                 orderItems.push(itemInfo)
             }
-        })
+        });
 
         let orderData = {
             address: data,
             items: orderItems,
             amount: getTotalCartAmount() + 40
-        }
+        };
 
         let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
 
         if (response.data.success) {
             const { session_url } = response.data;
-            window.location.replace(session_url)
-        }
-        else {
-            alert("Error")
+            window.location.replace(session_url);
+        } else {
+            alert("Error");
         }
     }
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/cart');
+        } else if (getTotalCartAmount() === 0) {
+            navigate('/cart');
+        }
+    }, [token, getTotalCartAmount, navigate]);
 
     return (
         <form onSubmit={placeOrder} className='place-order'>
